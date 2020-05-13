97f83692be9a52780c364942fb93349993338638
Xinyao Niu <morryniu123@gmail.com>
2020-05-13 22:54:42 +1000

    add shapes

--- src/Tools/Rectangle.java
+++ src/Tools/Rectangle.java
@@ -2,8 +2,9 @@ package Tools;
 
 import java.awt.*;
 
-public class Line extends Drawable {
-    public Line(Point start, Point end) {
+public class Rectangle extends Drawable{
+
+    public Rectangle(Point start, Point end) {
         super(start, end);
     }
 
@@ -11,6 +12,7 @@ public class Line extends Drawable {
     public void draw(Graphics2D g2d) {
         g2d.setColor(COLOR);
         g2d.setStroke(new BasicStroke(STROKE, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL));
-        g2d.drawLine(start.x, start.y, end.x, end.y);
+        g2d.drawRect(Math.min(start.x, end.x), Math.min(start.y, end.y),
+                Math.abs(start.x - end.x), Math.abs(start.y - end.y));
     }
 }
